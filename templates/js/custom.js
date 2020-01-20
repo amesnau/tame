37fea4601ae2b96ec2de121edb8927d5988eab37
Francisco Diaz <amesnau@gmail.com>
2020-01-20 22:30:25 +0100

    modificaciones a ejemplo en templates

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -1,2 +1,27 @@
 /* Add your custom Javascrit here */
-
+function cargaInicial() {
+document.getElementById('textoCambiar').innerHTML = 'Prueba';
+}
+window.onload = function() {
+  document.getElementById('textoCambiar').innerHTML = 'Prueba';
+}
+window.onpaint= funcion;
+function myFunction() {
+    document.getElementById('textoCambiar').innerHTML = '';
+    document.getElementById('textoCambiarOtro').innerHTML = 'Ayudamos a que alcances tus objetivos'
+    }
+function myFunction1() {
+    document.getElementById('textoCambiar').innerHTML = 'Estás en Portfolio';
+    document.getElementById('textoCambiarOtro').innerHTML = 'Portfolio';
+    }
+function myFunction2() {
+    document.getElementById('textoCambiar').innerHTML = 'Estás en nosotros';
+    document.getElementById('textoCambiarOtro').innerHTML = 'Nosotros';
+    }
+function myFunction3() {
+    document.getElementById('textoCambiar').innerHTML = 'Estás en contactos';
+    document.getElementById('textoCambiarOtro').innerHTML = 'Contactos';
+    }
+function botonAprendemas() {
+        document.getElementById('textoCambiar').innerHTML = 'Hemos desarrollado esta web para que puedas deleitarte';
+    }
